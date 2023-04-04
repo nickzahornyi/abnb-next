bcc3a3d23453ec8b091e83b94ed5b55f6730ddce
Nick Zahornyi <nick_surveyor@ukr.net>
2023-04-04 23:55:55 +0300

    prisma and register logic

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -32,7 +32,7 @@ const RegisterModal = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     axios
-      .post("/api.register", data)
+      .post("/api/register", data)
       .then(() => {
         registerModal.onClose();
       })
