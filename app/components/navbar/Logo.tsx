16dbcc00a5e5cd4da64c0cab7b737a95930a4a32
Nick Zahornyi <nick_surveyor@ukr.net>
2023-04-10 22:26:14 +0300

    categories

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,11 +8,12 @@ const Logo = () => {
 
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="logo"
       className="hidden md:block cursor-pointer"
       height="100"
       width="100"
-			src="/images/logo.png"
+      src="/images/logo.png"
     />
   );
 };
