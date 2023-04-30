ee2a52d89d9b19ba0626c312ab001d65ce7f550e
Nick Zahornyi <nick_surveyor@ukr.net>
2023-04-30 19:40:15 +0300

    properties page

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -11,10 +11,20 @@ export interface IListingsParams {
   category?: string;
 }
 
-export default async function getListings() {
-//   params: IListingsParams
+export default async function getListings(params: IListingsParams) {
   try {
+    const {
+      userId,
+    } = params;
+
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: "desc",
       },
