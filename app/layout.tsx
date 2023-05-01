9c3e672046f81794cad2c2f37de1c4a95441d189
Nick Zahornyi <nick_surveyor@ukr.net>
2023-05-01 20:09:37 +0300

    filters

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import { Nunito } from "next/font/google";
 
-import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import LoginModal from "./components/modals/LoginModal";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
+
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+
+import "./globals.css";
 
 export const metadata = {
   title: "Airbnb",
@@ -33,6 +36,7 @@ export default async function RootLayout({
           <LoginModal />
           <RegisterModal />
           <RentModal />
+          <SearchModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">{children}</div>
